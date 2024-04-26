c88a09713ab110dce966d9c9d732cbf78bafce7f
canshazin <shazinshazz@gmail.com>
2024-04-26 15:03:31 +0530

    route separation

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
+const adminRoutes = require(".\\routes\\admin.js");
+const shopRoutes = require(".\\routes\\shop.js");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" id="title" name="title"><input type="number" name="size" id="size"><button type="submit">Add Product</button></form>'
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
 app.use("/", (req, res, next) => {
-  res.send("<h1>HOME PAGE</H1>");
+  res.status(404).send("page not found");
 });
 
 app.listen(3000);
